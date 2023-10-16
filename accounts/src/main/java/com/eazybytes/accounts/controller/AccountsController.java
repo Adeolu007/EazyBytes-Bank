2d6df0cc37d9f0ef31db19ca64e344b2c0557603
Adeolu.Odunuyi <odunuyiadeolu@gmail.com>
2023-10-16 15:27:50 +0100

    completed the entities, worked on dtos, mapper, repositories, service and controller

---
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -0,0 +1,32 @@
+package com.eazybytes.accounts.controller;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.media.Content;
+//import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import jakarta.validation.Valid;
+//import jakarta.validation.constraints.Pattern;
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.dto.ResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
+public class AccountsController {
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount( @RequestBody CustomerDto customerDto) {
+//        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+}
---
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.eazybytes.accounts.service.impl;
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.AccountsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+//import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+//import com.eazybytes.accounts.exception.ResourceNotFoundException;
+//import com.eazybytes.accounts.mapper.AccountsMapper;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl  implements IAccountsService {
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+}
